cfce9d0eb04718e70bb82238aafc1dc7e4019af8
Piotr Orłowski <piotr1969.fi@gmail.com>
2024-06-23 21:12:44 +0200

    script now working

--- script.js
+++ script.js
@@ -1,37 +1,37 @@
-// user inserts a sentence
-// every letter that occurs more than once gets crossed out
-// the output is the set of unique letters 
+    // user inserts a sentence
+    // every letter that occurs more than once gets crossed out
+    // the output is the set of unique letters 
 
-function findUniqueLetters(){
-        
-    // declare array of all letters in your language
-    const letters = ["a", "ą", "b", "c", "ć", "d", "e", "ę", 
-        "f", "g", "h", "i", "j", "k", "l", "ł", "m", "n", "ń", "o",  
-        "ó", "p", "q", "r", "s", "ś", "t", "u", "v", "w", "x", "y", "z", "ż", "ź"];
+    function findUniqueLetters(){
+            
+        // declare array of all letters in your language
+        const letters = ["a", "ą", "b", "c", "ć", "d", "e", "ę", 
+            "f", "g", "h", "i", "j", "k", "l", "ł", "m", "n", "ń", "o",  
+            "ó", "p", "q", "r", "s", "ś", "t", "u", "v", "w", "x", "y", "z", "ż", "ź"];
 
-    // get user input and convert to lower case
-    const input = document.getElementById("userInput").value.toLowerCase();
+        // get user input and convert to lower case
+        const input = document.getElementById("userInput").value.toLowerCase();
 
-    // declare temporary arrays
-    let lettersFromInput = [];
-    let repeatedLetters = [];
+        // declare temporary arrays
+        let lettersFromInput = [];
+        let repeatedLetters = new Set();
 
-    // main loop
-    for (const char of input){
-        // check if char is a letter 
-        if(char.match(letters) ){
-            lettersFromInput.push(char);
-
-            // AND if it is not already in the temp arr
-            if((char.match(lettersFromInput)))
-
-            arr.push(char);
-
+        // main loop
+        for (const char of input){
+            // check if char is a letter 
+            if(letters.includes(char)){
+                // and if it's already pushed
+                if((lettersFromInput.includes(char))){
+                    repeatedLetters.add(char);
+                }
+                else{
+                    lettersFromInput.push(char);
+                }
+            }
         }
-    }
 
-    let difference = arr1.filter(x => !arr2.includes(x));
-    document.getElementById('result').textContent = `Unikalne litery: ${sortedUniqueLetters.join(', ')}`;
-}
+        let uniqueLetters = lettersFromInput.filter(x => !repeatedLetters.has(x));
+        document.getElementById('result').textContent = `Unikalne litery: ${uniqueLetters.join(', ')}`;
+    }
 
 
